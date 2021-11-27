ad5883a38d72026c675a48e3ed948eb587379db8
yashar6846 <yasharnajafi6846@gmail.com>
2021-11-27 15:43:51 +0300

    client

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const cors = require("cors");
 const GraphQLSchema = require("./schema");
 
 const app = express();
+
+//Allow cross-origin
+app.use(cors());
 
 app.use(
   "/graphql",
