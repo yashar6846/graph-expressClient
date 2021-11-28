c69796fccee7554f51835fd964e312a9ee045110
yashar6846 <yasharnajafi6846@gmail.com>
2021-11-28 22:43:42 +0300

    install heroko graphql express

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import logo from "./logo.png";
 import ApolloClient from "apollo-boost";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
 import Launches from "./components/Launches";
+import Launch from "./components/Launch";
 
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
@@ -11,14 +13,18 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="container">
-        <img
-          src={logo}
-          alt="SpaceX"
-          style={{ width: 300, display: "block", margin: "auto" }}
-        />
-        <Launches />
-      </div>
+      <Router>
+        <div className="container">
+          <img
+            src={logo}
+            alt="SpaceX"
+            style={{ width: 300, display: "block", margin: "auto" }}
+          />
+          <Routes exact path="/" component={Launches} />
+          <Routes exact path="/launch/:flight_number" component={Launch} />
+          <Launches />
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
